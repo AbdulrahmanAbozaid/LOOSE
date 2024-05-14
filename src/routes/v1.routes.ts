d458efde3e410857dfc1e409a491f5fd64e62741
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-05-14 23:02:09 +0300

    [OrdersModel]: data_model, controller, routes

--- src/routes/v1.routes.ts
+++ src/routes/v1.routes.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import UserRoutes from "./user/routes"
+import ProductRoutes from "./product/routes";
+import CategoryRoutes from "./category/routes";
+import OrderRoutes from "./order/routes";
+
 const app: Router = Router();
 
 app.use("/users", UserRoutes);
+app.use("/products", ProductRoutes);
+app.use("/categories", CategoryRoutes);
+app.use("/orders", OrderRoutes);
 
 export default app;
