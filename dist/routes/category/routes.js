657d8454f20fcf8dde2961c872bb12ec9f85300d
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-05-19 23:57:55 +0300

    [Products]: Search

--- dist/routes/category/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import categoryController from "../../controller/category.controller.js"; // import { authorize, restrictTo } from '../../utils/auth.service';
-import FileUploader from "../../middlewares/file_uploader.js";
-var router = express.Router();
-var upload = FileUploader();
-router.route('/').all().post(upload.single("thumbnail"), categoryController.createCategory).get(categoryController.getAllCategories);
-router.route('/:id').all().get(categoryController.getCategory).patch(upload.single("thumbnail"), categoryController.updateCategory)["delete"](categoryController.deleteCategory);
-router.put('/:id', categoryController.draftCategory);
-export default router;
